2356eb006873bf13a1c2f2cb0736ef1c8dda37e5
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-15 20:25:49 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -15,13 +15,12 @@ end
 
 def retrieval
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-shipping_manifest["oil paintings"]
-  #your code here!
+  shipping_manifest["oil paintings"]
 end
 
 def adding
   shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
-
+  shipping_manifest["pearl necklace"]=1
   #your code here
   #remember to return the shipping_manifest hash
 
