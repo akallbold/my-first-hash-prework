e9a3397b435d7f397242221e6e0058727be4f2ee
Anna Kallenborn-Bolden <a.kallenbornbolden@gmail.com>
2017-11-15 20:23:36 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -14,7 +14,7 @@ shipping_manifest = {"whale bone corset"=>5,"porcelain vases"=>2,"oil paintings"
 end
 
 def retrieval
-  shipping_manifest = {"whale bone corsets" => 5, "porcelain vases" => 2, "oil paintings" => 3}
+  shipping_manifest = {"whale bone corset" => 5, "porcelain vases" => 2, "oil paintings" => 3}
 
   #your code here!
 end
